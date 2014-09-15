51ccc7057ede422fc229cd19c7afe0ab4c0e7fd4
Robert <Robert@192.168.88.23>
2014-09-15 21:43:05 +0200

    for github

--- springLoadDataApp/springLoadDataApp/src/main/java/at/prasnikar/robert/springloaddataapp/controller/manufacturerControllerImpl.java
+++ springLoadDataApp/springLoadDataApp/src/main/java/at/prasnikar/robert/springloaddataapp/controller/manufacturerControllerImpl.java
@@ -8,11 +8,15 @@ package at.prasnikar.robert.springloaddataapp.controller;
 
 import at.prasnikar.robert.springloaddataapp.domain.manufacturer;
 import at.prasnikar.robert.springloaddataapp.service.manufacturerService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Robert
  */
+@RestController
 public class manufacturerControllerImpl implements manufacturerController {
     private manufacturerService m;
 
@@ -21,7 +25,7 @@ public class manufacturerControllerImpl implements manufacturerController {
      * @return
      */
     @Override
-    public manufacturerService getManufacturerService() {
+     public manufacturerService getManufacturerService() {
         return this.m;
     }
     
@@ -33,11 +37,22 @@ public class manufacturerControllerImpl implements manufacturerController {
         System.out.println("init manufacturerControllerImpl.");
     }
 
+    @Override
     public boolean add(manufacturer mf) {
         m.createManufacturer(mf);
         return true;//To change body of generated methods, choose Tools | Templates.
     }
 
+    /**
+     *
+     * @param l
+     * @return
+     */
+    @RequestMapping("/manufacturer")
+    public manufacturer manufacturerGetFirst(@RequestParam(value="id") Long l) {
+        return m.getManufacturer(l);
+    }
+    
     @Override
     public void print() {
         m.print(); //To change body of generated methods, choose Tools | Templates.
